0594ce8370f27f597aa5851ea46f4846c34b9bbb
Sauriel <sascha.muenchow@gmx.de>
2024-05-08 16:16:25 +0200

    removed expired api key

--- src/components/ui/FontFamilyPicker.vue
+++ src/components/ui/FontFamilyPicker.vue
@@ -5,12 +5,12 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
 import FontPicker from "@/font-picker/FontPicker";
 import { Font } from "@samuelmeuli/font-manager";
 import VuexHelper from "@/helper/VuexHelper";
-import AppDocument from '@/models/AppDocument';
-import { APP_MESSENGER } from '@/AppMessenger';
+import AppDocument from "@/models/AppDocument";
+import { APP_MESSENGER } from "@/AppMessenger";
 
 const appStore = VuexHelper.createNamespace("appStore");
 
@@ -21,7 +21,7 @@ export default class FontFamilyPicker extends Vue {
   @Prop(String)
   readonly value!: string;
 
-  @Prop({ type: String, default: "Open Sans"})
+  @Prop({ type: String, default: "Open Sans" })
   readonly default!: string;
 
   private fontPicker: FontPicker | null = null;
@@ -36,20 +36,20 @@ export default class FontFamilyPicker extends Vue {
 
   mounted() {
     this.fontPicker = new FontPicker(
-      "AIzaSyCkLIuemWkzC0u_ARHtv_FsmYyXAETl0XM", // Google API key
+      "<YOUR_API_KEY_HERE>", // Google API key
       "Open Sans", // Default font
       {
         limit: 100, // 2021-04-16: There are currently 1052 google fonts, so i'm only loading the top 100
         pickerId: "custom",
-        scripts: ["latin", "latin-ext"]
-      }, // Additional options
+        scripts: ["latin", "latin-ext"],
+      } // Additional options
     );
     APP_MESSENGER.send("loadFonts");
-        APP_MESSENGER.on("fontsLoaded", (event, fonts) => {
-          if (this.fontPicker) {
-            this.fontPicker.addLocalFonts(fonts, true);
-          }
-        });
+    APP_MESSENGER.on("fontsLoaded", (event, fonts) => {
+      if (this.fontPicker) {
+        this.fontPicker.addLocalFonts(fonts, true);
+      }
+    });
     this.fontPicker.addLocalFonts(this.settingFonts, true);
     this.fontPicker.setActiveFont(this.default);
     this.fontPicker.setOnChange(this.fontChanged);
@@ -57,7 +57,9 @@ export default class FontFamilyPicker extends Vue {
 
   @Watch("value")
   onValueChanged() {
-    const fontPickerActiveFont = this.fontPicker ? this.fontPicker.getActiveFont().family : undefined;
+    const fontPickerActiveFont = this.fontPicker
+      ? this.fontPicker.getActiveFont().family
+      : undefined;
     if (this.internalValue !== fontPickerActiveFont && this.fontPicker) {
       this.fontPicker.setActiveFont(this.internalValue);
     }
@@ -68,49 +70,51 @@ export default class FontFamilyPicker extends Vue {
   }
 
   private get settingFonts(): string[] {
-    const document: AppDocument =  appStore.getters<AppDocument>("document");
+    const document: AppDocument = appStore.getters<AppDocument>("document");
     return document.settings.setting.fonts;
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  @import "@/style/colors.scss";
+@import "@/style/colors.scss";
 
-  .font-picker {
-    display: inline-block;
+.font-picker {
+  display: inline-block;
 
-    #font-picker-custom {
-      width: auto;
-      min-width: 150px;
+  #font-picker-custom {
+    width: auto;
+    min-width: 150px;
 
-      &::v-deep {
-        > .dropdown-button {
-          background-color: $backgroundDark;
-          height: 25px;
+    &::v-deep {
+      > .dropdown-button {
+        background-color: $backgroundDark;
+        height: 25px;
 
-          .dropdown-icon.finished:before {
-            border-top-color: $foreground;
-          }
+        .dropdown-icon.finished:before {
+          border-top-color: $foreground;
         }
+      }
 
-        > ul {
-          background-color: $backgroundLight;
+      > ul {
+        background-color: $backgroundLight;
 
-          > li {
-            > button {
-              &.active-font, &:hover, &:focus {
-                background-color: $foregroundDark;
-                color: $backgroundDark;
-              }
+        > li {
+          > button {
+            &.active-font,
+            &:hover,
+            &:focus {
+              background-color: $foregroundDark;
+              color: $backgroundDark;
+            }
 
-              &.active-font {
-                color: $primary;
-              }
+            &.active-font {
+              color: $primary;
             }
           }
         }
       }
     }
   }
+}
 </style>
